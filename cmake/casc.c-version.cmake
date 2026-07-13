caa92c1739cac402b66f8fdbb07c9bdfa99b71dc
Caspian <caspianrz@gmail.com>
2026-07-13 13:04:32 +0330

    add base build skeleton.

---
+++ cmake/casc.c-version.cmake
@@ -0,0 +1,9 @@
+set(casc.c_version_major 1)
+set(casc.c_version_minor 0)
+set(casc.c_version_patch 0)
+
+set(casc.c_version
+    "${casc.c_version_major}.${casc.c_version_minor}.${casc.c_version_patch}"
+)
+
+set(PACKAGES_VERSION ${casc.c_version})
---
+++ src/casc.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int casc_c_main(int argc, char *argv[])
+{
+    fprintf(stderr, "casc.c: is not implemented yet.\n");
+    exit(EXIT_FAILURE);
+
+    return EXIT_FAILURE;
+}
+
+int main(int argc, char *argv[])
+{
+    return casc_c_main(argc, argv);
+}
